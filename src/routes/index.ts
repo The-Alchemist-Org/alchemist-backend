6e96c33ebd5af301e7784bcc3ffdeebb387cad85
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2024-04-15 17:06:25 +0200

    Add routes/index.ts file for registering routes in the application

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Application } from 'express';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- TODO Add routes per domain
+const registerRoutes = (app: Application) => {
+};
+
+export default registerRoutes;
