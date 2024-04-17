2163afb973600733de2df37bc81de4481b09025a
FiskFisk33 <github@tehfisk.se>
2024-04-17 14:13:02 +0200

    Corrected the Drink config model. now one row in the relation is used per dispenser.

--- src/database/migrations/1713348467938-DrinkConfigModel.ts
+++ src/database/migrations/1713348467938-DrinkConfigModel.ts
@@ -11,88 +11,32 @@ export class DrinkConfigModel1713348467938 implements MigrationInterface {
 						name: 'id',
 						type: 'int',
 						isPrimary: true,
-						generationStrategy: 'increment'
 					},
 					{
-						name: 'dispenser_1_ingredient',
+						name: 'ingredient',
 						type: 'int'
 					},
 					{
-						name: 'dispenser_2_ingredient',
+						name: 'amount_left',
 						type: 'int'
 					},
-					{
-						name: 'dispenser_3_ingredient',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_4_ingredient',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_5_ingredient',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_1_amount_left',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_2_amount_left',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_3_amount_left',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_4_amount_left',
-						type: 'int'
-					},
-					{
-						name: 'dispenser_5_amount_left',
-						type: 'int'
-					}
 
 				],
 				foreignKeys: [
 					{
-						name: 'dispenser_1_ingredient',
+						name: 'ingredient',
 						referencedTableName: 'ingredients',
 						referencedColumnNames: ['id'],
-						columnNames: ['dispenser_1_ingredient']
+						columnNames: ['ingredient']
 					},
-					{
-						name: 'dispenser_2_ingredient',
-						referencedTableName: 'ingredients',
-						referencedColumnNames: ['id'],
-						columnNames: ['dispenser_2_ingredient']
-					},
-					{
-						name: 'dispenser_3_ingredient',
-						referencedTableName: 'ingredients',
-						referencedColumnNames: ['id'],
-						columnNames: ['dispenser_3_ingredient']
-					},
-					{
-						name: 'dispenser_4_ingredient',
-						referencedTableName: 'ingredients',
-						referencedColumnNames: ['id'],
-						columnNames: ['dispenser_4_ingredient']
-					},
-					{
-						name: 'dispenser_5_ingredient',
-						referencedTableName: 'ingredients',
-						referencedColumnNames: ['id'],
-						columnNames: ['dispenser_5_ingredient']
-					}
+
 				]
 			})
 		)
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
-        	await queryRunner.dropTable('drink_config');
+		await queryRunner.dropTable('drink_config');
 	}
 
 }
--- src/entities/drink-config.ts
+++ src/entities/drink-config.ts
@@ -1,62 +1,25 @@
 import {
-    Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn,
-   } from 'typeorm';
+	Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn,
+} from 'typeorm';
 import { Ingredient } from './ingredient';
-   
-   export interface IDrinkConfig {
-     id: number;
-     dispenser1Ingredient: Ingredient;
-     dispenser2Ingredient: Ingredient;
-     dispenser3Ingredient: Ingredient;
-     dispenser4Ingredient: Ingredient;
-     dispenser5Ingredient: Ingredient;
-     dispenser1AmountLeft: number;
-     dispenser2AmountLeft: number;
-     dispenser3AmountLeft: number;
-     dispenser4AmountLeft: number;
-     dispenser5AmountLeft: number;
-   }
-   
-   @Entity('recipes')
-   export class Recipe implements IDrinkConfig {
-     @PrimaryGeneratedColumn( { name: 'id' } )
-     id: number;
 
-     @OneToOne(() => Ingredient)
-     @JoinColumn()
-     dispenser1Ingredient: Ingredient;
+export interface IDrinkConfig {
+	id: number;
+	ingredient: Ingredient;
+	amountLeft: number;
+}
 
-     @OneToOne(() => Ingredient)
-     @JoinColumn()
-     dispenser2Ingredient: Ingredient;
+@Entity('drink_config')
+export class DrinkConfig implements IDrinkConfig {
+	@PrimaryGeneratedColumn({ name: 'id' })
+	id: number;
 
-     @OneToOne(() => Ingredient)
-     @JoinColumn()
-     dispenser3Ingredient: Ingredient;
+	@OneToOne(() => Ingredient)
+	@JoinColumn()
+	ingredient: Ingredient;
 
-     @OneToOne(() => Ingredient)
-     @JoinColumn()
-     dispenser4Ingredient: Ingredient;
+	@Column({ name: 'amount_left' })
+	amountLeft: number;
 
-     @OneToOne(() => Ingredient)
-     @JoinColumn()
-     dispenser5Ingredient: Ingredient;
+}
 
-     @Column( { name: 'dispenser_1_amount_left' } )
-     dispenser1AmountLeft: number;
-
-     @Column( { name: 'dispenser_2_amount_left' } )
-     dispenser2AmountLeft: number;
-
-     @Column( { name: 'dispenser_3_amount_left' } )
-     dispenser3AmountLeft: number;
-
-     @Column( { name: 'dispenser_4_amount_left' } )
-     dispenser4AmountLeft: number;
-
-     @Column( { name: 'dispenser_5_amount_left' } )
-     dispenser5AmountLeft: number;
-
-
-   }
-   
