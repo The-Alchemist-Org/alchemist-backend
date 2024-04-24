d7acb293899ee18836d5fd184bad748f91f12a15
FiskFisk33 <github@tehfisk.se>
2024-04-24 09:08:19 +0200

    fixed indentation with eslint

--- src/database/migrations/1713430057593-RatingModel.ts
+++ src/database/migrations/1713430057593-RatingModel.ts
@@ -1,55 +1,53 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class RatingModel1713430057593 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'ratings',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'recipe_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'rating',
+            type: 'int',
+            isNullable: false,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'recipe_id',
+            referencedTableName: 'recipes',
+            referencedColumnNames: ['id'],
+            columnNames: ['recipe_id'],
+          },
+          {
+            name: 'user_id',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+          },
+        ],
+      }),
+    );
+  }
 
-	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.createTable(
-			new Table({
-				name: 'ratings',
-				columns: [
-					{
-						name: 'id',
-						type: 'int',
-						isPrimary: true,
-						isGenerated: true,
-						generationStrategy: 'increment'
-					},
-					{
-						name: 'recipe_id',
-						type: 'int',
-						isNullable: false
-					},
-					{
-						name: 'user_id',
-						type: 'int',
-						isNullable: false
-					},
-					{
-						name: 'rating',
-						type: 'int',
-						isNullable: false,
-					},
-				],
-				foreignKeys: [
-					{
-						name: 'recipe_id',
-						referencedTableName: 'recipes',
-						referencedColumnNames: ['id'],
-						columnNames: ['recipe_id']
-					},
-					{
-						name: 'user_id',
-						referencedTableName: 'users',
-						referencedColumnNames: ['id'],
-						columnNames: ['user_id']
-					},
-				]
-			})
-		)
-	}
-
-	public async down(queryRunner: QueryRunner): Promise<void> {
-        	await queryRunner.dropTable('ratings');
-	}
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('ratings');
+  }
 }
--- src/entities/rating.ts
+++ src/entities/rating.ts
@@ -1,30 +1,29 @@
 import {
-    Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn
-   } from 'typeorm';
+  Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn,
+} from 'typeorm';
+import { User } from './user';
+import { Recipe } from './recipe';
 
-   export interface IRating {
-     id: number;
-     recipe: Recipe;
-     user: User;
-     rating: number;
-   }
-   import { User } from './user';
-   import { Recipe } from './recipe';
-   
-   @Entity('ratings')
-   export class Rating implements IRating {
-     @PrimaryGeneratedColumn( { name: 'id' } )
-     id: number;
-     
-     @OneToOne(() => User)
-     @JoinColumn()
-     recipe: Recipe;
+export interface IRating {
+  id: number;
+  recipe: Recipe;
+  user: User;
+  rating: number;
+}
 
-     @OneToOne(() => Recipe)
-     @JoinColumn()
-     user: User;
+@Entity('ratings')
+export class Rating implements IRating {
+  @PrimaryGeneratedColumn({ name: 'id' })
+    id: number;
 
-     @Column( { name: 'rating' } )
-     rating: number;
-   }
-   
+  @OneToOne(() => User)
+  @JoinColumn()
+    recipe: Recipe;
+
+  @OneToOne(() => Recipe)
+  @JoinColumn()
+    user: User;
+
+  @Column({ name: 'rating' })
+    rating: number;
+}
